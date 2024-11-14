099e3af4344c643be7a69c0e995e797a92303a22
Shane Riddell <shane.riddell@ellucian.com>
2024-11-14 12:53:44 -0500

    Render json to file

--- renderers/json/json.go
+++ renderers/json/json.go
@@ -3,9 +3,11 @@ package json
 
 import (
 	"encoding/json"
-	"fmt"
+	"log"
 
 	"github.com/devops-kung-fu/bomber/models"
+	"github.com/devops-kung-fu/common/util"
+	"github.com/spf13/afero"
 )
 
 // Renderer contains methods to render to JSON format
@@ -14,6 +16,25 @@ type Renderer struct{}
 // Render outputs json to STDOUT
 func (Renderer) Render(results models.Results) error {
 	b, _ := json.MarshalIndent(results, "", "\t")
-	fmt.Println(string(b))
-	return nil
+	var afs *afero.Afero
+
+	if results.Meta.Provider == "test" {
+		afs = &afero.Afero{Fs: afero.NewMemMapFs()}
+	} else {
+		afs = &afero.Afero{Fs: afero.NewOsFs()}
+	}
+
+	filename := "bomber-results.json"
+	util.PrintInfo("Writing JSON output:", filename)
+	file, err := afs.Create(filename)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	if _, err := file.Write(b); err != nil {
+		log.Println(err)
+		return err
+	}
+	err = afs.Fs.Chmod(filename, 0644)
+	return err
 }
